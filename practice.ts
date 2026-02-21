c2aee45989436c57e68ae3b857624b038df1100b
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-02-22 00:45:07 +0530

    Initial commit

---
+++ practice.ts
@@ -0,0 +1,16 @@
+// Welcome to TypeScript practice!
+// You can run this file directly using ts-node:
+// `ts-node practice.ts`
+// Or compile it to JavaScript using tsc:
+// `tsc practice.ts`
+
+let isAwesome: boolean = true;
+let greeting: string = "Hello, TypeScript!";
+let num: number = 42;
+
+function greet(name: string): string {
+    return `${greeting} My name is ${name}.`;
+}
+
+console.log(greet("Developer"));
+console.log(`TypeScript is awesome? ${isAwesome}`);
---
+++ testing.ts
@@ -0,0 +1,11 @@
+let id = 0;
+// id ="nigjsdfnk"; //this will give an error because id is of type number
+console.log(id);
+
+// this will work because model is of type string or number
+// union type
+var model: string| number = "BMW";
+
+let arr= [1,2,3,4,5];
+
+
